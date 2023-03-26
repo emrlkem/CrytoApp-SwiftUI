fb3a90b406bcb010e99c8b81790c6d8d96425b13
Emre Ülkem <98457972+emrlkem@users.noreply.github.com>
2023-03-26 23:06:11 +0300

    Add files via upload

---
+++ +Crypto/+Crypto/Extensions/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  +Crypto
+//
+//  Created by Emre Ülkem on 25.03.2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
---
+++ +Crypto/+Crypto/_CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  _CryptoApp.swift
+//  +Crypto
+//
+//  Created by Emre Ülkem on 23.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct _CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
