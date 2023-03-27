2c4184ec6082e5ed1fe2d621b31b68e17c4fba75
Emre Ülkem <98457972+emrlkem@users.noreply.github.com>
2023-03-27 23:37:14 +0300

    Add files via upload
    Creating HomeView and a navigation header

---
+++ +Crypto/+Crypto/Core/Views/HomeView.swift
@@ -0,0 +1,66 @@
+//
+//  HomeView.swift
+//  +Crypto
+//
+//  Created by Emre Ülkem on 27.03.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            // background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // content layer
+            VStack {
+                homeHeader
+                
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+    }
+}
+
+extension HomeView {
+    
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+    
+}
--- +Crypto/+Crypto/_CryptoApp.swift
+++ +Crypto/+Crypto/_CryptoApp.swift
@@ -11,7 +11,12 @@ import SwiftUI
 struct _CryptoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
+
+
