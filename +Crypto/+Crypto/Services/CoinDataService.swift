9b2a1232cc0325fa2b18386e3ffe9dafbdea0083
Emre Ülkem <98457972+emrlkem@users.noreply.github.com>
2023-05-22 00:33:10 +0300

    Add files via upload
    Download coins using a View Model and Data Service class

--- +Crypto/+Crypto/Services/CoinDataService.swift
+++ +Crypto/+Crypto/Services/CoinDataService.swift
@@ -6,10 +6,13 @@
 //
 
 import Foundation
+import Combine
 
 class CoinDataService {
     
     @Published var allCoins: [CoinModel] = []
+    
+    var coinSubscription: AnyCancellable?
     
     init() {
         getCoins()
@@ -19,7 +22,7 @@ class CoinDataService {
         
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
         
-        URLSession.shared.dataTaskPublisher(for: url)
+        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
             .tryMap { (output) -> Data in
                 guard let response = output.response as? HTTPURLResponse,
@@ -31,9 +34,16 @@ class CoinDataService {
             .receive(on: DispatchQueue.main)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
             .sink { (completion) in
-                
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print(error.localizedDescription )
+                }
+            } receiveValue: { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+                self?.coinSubscription?.cancel()
             }
-        
         
     }
     
