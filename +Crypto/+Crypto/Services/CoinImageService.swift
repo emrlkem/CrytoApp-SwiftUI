fbcac0c1e97f1c79ec80705436bce24337737c9f
Emre Ülkem <98457972+emrlkem@users.noreply.github.com>
2023-05-28 16:54:59 +0300

    Add files via upload
    Downloading Coin Images using Combine, MVVM, and a Networking Layer.

---
+++ +Crypto/+Crypto/Services/CoinImageService.swift
@@ -0,0 +1,22 @@
+//
+//  CoinImageService.swift
+//  +Crypto
+//
+//  Created by Emre Ülkem on 28.05.2023.
+//
+
+import Foundation
+import SwiftUI
+
+class CoinImageService {
+    
+    @Published var image: UIImage? = nil
+    
+    init() {
+        getCoinImage()
+    }
+    
+    private func getCoinImage() {
+        
+    }
+}
